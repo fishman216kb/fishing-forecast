d449c6c8ee3b73a413ff2bf82667f2e5e4fa5eee
fishman216kb <fishman216kb@gmail.com>
2025-05-18 15:19:30 -0400

    Update reformat_forecast.py

--- reformat_forecast.py
+++ reformat_forecast.py
@@ -49,7 +49,7 @@ for line in lines[start_index:]:
     if re.match(r".*\d{1,2}(:\d{2})? ?(AM|PM)", line):
         html_parts.append(f"{line}<br><br>")
         continue
-    if line.startswith("...") in line:
+    if line.startswith("...") and "" in line:
         html_parts.append(f"<advisory>{line}</advisory><br><br>")
         continue
     if line.startswith(".") and "..." in line:
